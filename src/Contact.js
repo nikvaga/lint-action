6e2d08ed4664ebcc102e37ab207c001c5b997711
nikunj <nikunjvagadiya12@gmail.com>
2023-03-30 15:28:25 +0530

    added new json

--- src/Contact.js
+++ src/Contact.js
@@ -1,14 +1,15 @@
-// import logo from './logo.svg';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import  React from "react";
+
 function Contact() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-    Hey Contact with me on this number.
+          Hey Contact with me on this number.
         </p>
-      
+        {logo}
       </header>
     </div>
   );
