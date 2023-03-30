1c9c47fa262a200a5cb3360aa8dd2a5554c4afa2
nikunj <nikunjvagadiya12@gmail.com>
2023-03-30 17:07:25 +0530

    add useEffect

--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function Contact() {
+  const [count, setCount] = useState(0)
+  let a = 10
+  
+  useEffect(()=>{
+    setCount(count+1)
+  }, [])
+  
   return (
     <div className="App">
       <header className="App-header">
